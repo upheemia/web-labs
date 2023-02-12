df9a4ea03987d87588270c32a199feba30a97cea
Alina <alinader02@mail.ru>
2023-02-12 13:56:59 +0300

    react-third-lab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import { List } from './components/List/List';
+import { useEffect, useState } from 'react';
+
+const initialData = [
+  { id: 1, content: 'first element' },
+  { id: 2, content: 'second element' },
+  { id: 3, content: 'third element' },
+  { id: 4, content: 'fourth element' },
+  { id: 5, content: 'fifth element' },
+]
 
 function App() {
-  const data = [
-    { id: 1, content: 'first element' },
-    { id: 2, content: 'second element' },
-    { id: 3, content: 'third element' },
-  ]
+  const [searchValue, setSearchValue] = useState('');
+  const [data, setData] = useState(initialData)
   
+  useEffect(() => {
+    const filteredData = initialData.filter(item => item.content.includes(searchValue));
+    setData(filteredData)
+
+    if (!searchValue.length) {
+      document.title = "useEffect hook example"
+    } else {
+      document.title = `${data.length} элементов в списке`
+    }
+  }, [searchValue, data])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +41,13 @@ function App() {
         >
           Learn React
         </a>
-        <List data={data}/>
+        <input
+          type="text"
+          className='search-input'
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+        />
+        <List data={data} searchSubs={searchValue}/>
       </header>
     </div>
   );
